25de0e02fd8e3929876a40e2d822b0cd89871c4a
Jeffrey Izelaar <jizelaar@gmail.com>
2021-07-14 11:20:30 +0200

    Add accordion text. Add accordion placeholder styles and behaviour.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-// This script allows for the accordions to open and collapse
+// This script allows for the accordions to open and collapse. Note that the first toggler starts as "active"
+
+/**WHITEBOARD
+ * Click > if already active, remove active > if not active, add active, remove active from togglers that are not this togglers.
+ * 
+ */ 
 
 const togglers = document.getElementsByClassName("accordion__toggler")
 
