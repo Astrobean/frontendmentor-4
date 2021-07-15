42a224bdd2593039319ff725239775347e043853
Jeffrey Izelaar <jizelaar@gmail.com>
2021-07-15 10:57:54 +0200

    clean up deprecated files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// This script allows for the accordions to open and collapse. Note that the first toggler starts as "active"
-
-/**WHITEBOARD
- * Click > if already active, remove active > if not active, add active, remove active from togglers that are not this togglers.
- * 
- */ 
-
-// Scrapped!
