a159fbd813b8182019e36d9addf0853c47ea34cb
Jeffrey Izelaar <jizelaar@gmail.com>
2021-07-15 10:57:20 +0200

    add functional CSS-only accordion with content

--- js/script.js
+++ js/script.js
@@ -5,16 +5,4 @@
  * 
  */ 
 
-const togglers = document.getElementsByClassName("accordion__toggler")
-
-for(i = 0; i < togglers.length; i++) { // for each toggler that exists ...
-
-    togglers[i].addEventListener("click", function() { // ... wait until someone clicks on it ...
-
-        this.firstElementChild.classList.toggle("active") // ... then add the "active class to the toggler's first child element (accordion__title) ...
-        this.lastElementChild.classList.toggle("active") // ... to the toggler's last child element (accordion__arrow) ...
-        this.nextElementSibling.classList.toggle("active") // ... and finally to the toggler's sibling element (accordion__panel)!
-
-    })
-
-}
+// Scrapped!
